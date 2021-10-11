17357ec20ab9f2480b160b1a851e92770e7bfaf4
wijeremy <jeremydavwilliams@gmail.com>
2021-10-11 14:48:47 -0500

    going back from mern to just react

---
+++ portfolio.react1/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Home from './pages/Home';
+// import Matchup from './pages/Matchup';
+// import Vote from './pages/Vote';
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ portfolio.react1/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Nav from '../components/NavBar';
+import Accordion from '../components/Accordion';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <>
+      <Nav />
+      <Accordion />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
